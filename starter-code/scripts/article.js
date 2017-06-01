ebb43e4b720e1dab98e0fb145841d2fc920a1e21
Mark Petrie <petrie.mark@gmail.com>
2017-06-01 14:33:11 -0700

    Complete TODOs for lab-04.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -2,7 +2,7 @@
 
 var articles = [];
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
@@ -11,7 +11,9 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function () {
+  var templateFiller = Handlebars.compile($('#articleTemplate').html());
+
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
@@ -20,21 +22,24 @@ Article.prototype.toHtml = function() {
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+  this.publishStatus = this.publishedOn ? `${this.daysAgo} days ago` : '(draft)';
+
+  var filledTemplate = templateFiller ( this );
+  return filledTemplate;
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
 
 };
 
-rawData.sort(function(a,b) {
+rawData.sort(function (a, b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-rawData.forEach(function(articleObject) {
+rawData.forEach(function (articleObject) {
   articles.push(new Article(articleObject));
 });
 
-articles.forEach(function(article){
+articles.forEach(function (article) {
   $('#articles').append(article.toHtml());
 });
